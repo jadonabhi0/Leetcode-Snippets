efec01ee9da6b6c7630e57048a4a7b9dbd910983
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-03-14 21:15:00 +0530

    Time: 32 ms (68.27%), Space: 64.6 MB (12.98%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.java
@@ -2,38 +2,38 @@ class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         
        
-        List<String> res = new ArrayList<>();
-        HashSet<String> hs = new HashSet<>();
-        for(int i = 0; i <= s.length()-10; i++) {
-            String subs = s.substring(i, i + 10);
-            if(hs.contains(subs)) {
-                if(!res.contains(subs)) {
-                    res.add(subs);
+//         List<String> res = new ArrayList<>();
+//         HashSet<String> hs = new HashSet<>();
+//         for(int i = 0; i <= s.length()-10; i++) {
+//             String subs = s.substring(i, i + 10);
+//             if(hs.contains(subs)) {
+//                 if(!res.contains(subs)) {
+//                     res.add(subs);
+//                 }
+//             }
+//             else {
+//                 hs.add(subs);
+//             }
+//         }
+//         return res;
+//     }
+        
+        
+        HashSet<String> set = new HashSet<>();
+        List<String> list = new LinkedList<>();
+        int start = 0, end = 9;
+        if(s.length()<10) return list;
+        while(end != s.length()){
+            String str = s.substring(start,end+1);
+            if(set.contains(str)){
+                if(!list.contains(str)){
+                    list.add(str);
                 }
+            }else{
+                set.add(str);
             }
-            else {
-                hs.add(subs);
-            }
+            end++;start++;
         }
-        return res;
+        return list;
     }
-        
-        
-    //     HashSet<String> set = new HashSet<>();
-    //     List<String> list = new LinkedList<>();
-    //     int start = 0, end = 9;
-    //     if(s.length()<10) return list;
-    //     while(end != s.length()-1){
-    //         String str = s.substring(start,end+1);
-    //         if(set.contains(str)){
-    //             if(!list.contains(str)){
-    //                 list.add(str);
-    //             }
-    //         }else{
-    //             set.add(str);
-    //         }
-    //         end++;start++;
-    //     }
-    //     return list;
-    // }
 }
