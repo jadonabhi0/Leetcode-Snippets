b35f0bbed3bc2ab41fb6bee519be94dd8a66c870
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-04-17 15:20:01 +0530

    Time: 27 ms (73.73%), Space: 47 MB (79.84%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,27 +1,37 @@
 class Solution {
-    
-    
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list = new LinkedList<>();
-        if(nums.length<3) return list;
+   public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
+        List<List<Integer>> ans = new LinkedList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
+        if (nums.length < 3) {
+            return  ans;
+        }
         for(int i = 0 ; i < nums.length-2 ; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            int a = nums[i];
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int low = i+1;
             int high = nums.length-1;
+            int tar = -nums[i];
             while(low < high){
-                if(nums[low]+nums[high] == -a){
-                    list.add(Arrays.asList(a,nums[low],nums[high]));
-                    while(low < high && nums[high] == nums[high-1]) high--;
-                    while(low < high && nums[low] == nums[low+1]) low++;
+                if(nums[low] + nums[high] == tar){
+                    list.add(nums[i]);
+                    list.add(nums[low]);
+                    list.add(nums[high]);
+                    set.add(list);
+                    list = new ArrayList<>();
+                    while (low < high && nums[low+1] == nums[low]) low++;
+                    while (low < high && nums[high-1] == nums[high]) high--;
                     low++;
                     high--;
                 }
-                else if(nums[low]+nums[high] > -a) high--;
-                else if(nums[low]+nums[high] < -a) low++;
+                else if(tar > nums[low]+nums[high]) low++;
+                else high--;
             }
         }
-        return list;
+
+        for(List<Integer> l : set){
+            ans.add(l);
+        }
+        return ans;
     }
 }
