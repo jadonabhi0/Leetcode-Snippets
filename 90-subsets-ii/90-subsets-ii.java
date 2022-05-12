891429c581442c3a6f3b958894e225bbde85a424
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-05-12 22:48:16 +0530

    Time: 1 ms (98.46%), Space: 42.4 MB (92.74%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+     public static List<List<Integer>> generateSubset(int[] array, int pos, List<Integer> list, List<List<Integer>> ans){
+//        if (pos == array.length){
+//            return ans;
+//        }
+        ans.add(new LinkedList<>(list));
+
+        for (int i = pos ; i < array.length ; i++){
+            if(i > pos && array[i] == array[i-1]) continue;
+            list.add(array[i]);
+            generateSubset(array, i+1, list, ans);
+            list.remove(list.size()-1);
+        }
+        return ans;
+    }
+    
+    
+    
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        return generateSubset(nums, 0,new ArrayList<>(), new ArrayList<>());
+    }
+}
