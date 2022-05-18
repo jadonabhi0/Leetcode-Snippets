2ab3f967397e15f3799ad4a88117804122b91c7c
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-05-18 23:36:51 +0530

    Time: 6 ms (71.10%), Space: 97.6 MB (49.11%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,22 +10,66 @@
  */
 class Solution {
     
+    public ListNode rev(ListNode node){
+        ListNode prev = null;
+        ListNode cur = node;
+        
+        while(cur != null){
+            ListNode store = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = store;
+            
+        }
+        return prev;
+        
+    }
+    
     
      public boolean isPalindrome(ListNode head) {
-        List<Integer> intList = new ArrayList<Integer>();
+         
+         //FIRST APPROACH
+         
+//         List<Integer> intList = new ArrayList<Integer>();
         
-        ListNode temp = head;
-        while(temp != null){
-            intList.add(temp.val);
-            temp = temp.next;
-        }        
+//         ListNode temp = head;
+//         while(temp != null){
+//             intList.add(temp.val);
+//             temp = temp.next;
+//         }        
         
-        for(int i=0, j=intList.size()-1; i<=j; i++, j--){
-            if(intList.get(i)!=intList.get(j)){
-                return false;
-            }
-        }
+//         for(int i=0, j=intList.size()-1; i<=j; i++, j--){
+//             if(intList.get(i)!=intList.get(j)){
+//                 return false;
+//             }
+//         }
         
-        return true;        
+//         return true;        
+         
+         
+        // SECOND APPROACH
+         
+         
+         
+         ListNode slow = head;
+         ListNode fast = head;
+         
+         while(fast != null && fast.next != null){
+             slow = slow.next;
+             fast = fast.next.next;
+         }
+         
+         ListNode temp1 = head;
+         ListNode temp2 = rev(slow);
+         
+         
+         while(temp1 != null && temp2 != null){
+             if(temp1.val != temp2.val){
+                 return false;
+             }
+             temp1=temp1.next;
+             temp2=temp2.next;
+         }
+         return true;
     }    
 }
