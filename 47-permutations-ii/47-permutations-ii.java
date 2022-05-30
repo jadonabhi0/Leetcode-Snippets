25a61c4f181695a3bf7f1612a4da1b4f2adec891
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-05-30 12:47:00 +0530

    Time: 7 ms (41.90%), Space: 49.5 MB (25.70%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+     public void perm(int[] array,int idx, List<Integer> list, List<List<Integer>> ans, boolean[] vis){
+        if(idx == array.length){
+            ans.add(new LinkedList<>(list));
+            return;
+        }
+        
+        for (int i = 0 ; i < array.length ; i++){
+            if (i > 0 && array[i] == array[i-1] && !vis[i-1]) continue;
+            if (vis[i]) continue;
+            vis[i] = true;
+            list.add(array[i]);
+            perm(array ,idx+1, list,ans,vis);
+            list.remove(list.size()-1);
+            vis[i] = false;
+        }
+    }
+    
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new LinkedList<>();
+        boolean[] vis = new boolean[nums.length];
+        perm(nums, 0, new LinkedList<>(), list,vis);
+    
+        return list;
+    }
+}
