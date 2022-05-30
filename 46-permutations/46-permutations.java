5b7fecbcde65b44cf5f0b82e8d2e2b36c3dbb9b0
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-05-30 12:37:18 +0530

    Time: 3 ms (37.25%), Space: 45.3 MB (13.65%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,72 +1,26 @@
 class Solution {
-//     static List<List<Integer>> result;
-//     static LinkedList<Integer> list;
-//    public Solution(){
-//         this.result = new LinkedList<>();
-//          this.list = new LinkedList<>();
-//    }
-//     static List<List<Integer>> perm(String str,String newStr){
-//         if(str.length() == 0){
-//                 for(int i = 0 ; i < newStr.length() ; i++){
-//                 int a = Integer.parseInt(newStr.charAt(i)+"");
-//                 list.add(a);
-//             }
-           
-//             result.add(list);
-//             list = new LinkedList<>();
-//             return result;
-//         }
-//         for(int i = 0 ; i < str.length() ; i++){
-//             char c = str.charAt(i);
-//             String sub = str.substring(0,i)+str.substring(i+1);
-//             perm(sub,newStr+c);
-//         }
-//         return result;
-//     }
     
-//     public List<List<Integer>> permute(int[] nums) {
-//         if (nums.length == 1){
-//             list.add(nums[0]);
-//             result.add(list);
-//             list = new LinkedList<>();
-//             return result;
-//         } 
-//         StringBuilder sb = new StringBuilder();
-//         for(int i : nums){
-//             sb.append(i);
-//         }
-//         String str = sb.toString();
-//         List<List<Integer>> listt = perm(str,"");
-//         return listt;
-     // }
-    
+    public void perm(int[] array,int idx, List<Integer> list, List<List<Integer>> ans, boolean[] vis){
+        if(idx == array.length){
+            ans.add(new LinkedList<>(list));
+            return;
+        }
+        
+        for (int i = 0 ; i < array.length ; i++){
+            if (vis[i]) continue;
+            vis[i] = true;
+            list.add(array[i]);
+            perm(array ,idx+1, list,ans,vis);
+            list.remove(list.size()-1);
+            vis[i] = false;
+        }
+    }
     
     public List<List<Integer>> permute(int[] nums) {
-		List<List<Integer>> allList = new ArrayList<>();
-		helper(nums, 0, new ArrayList<>(), allList);
-		return allList;
-	}
-
-	public void helper(int[] nums, int from, List<Integer> cur, List<List<Integer>> allList) {
-		if(cur.size() == nums.length) {
-			allList.add(new ArrayList<Integer>(cur));
-			return;
-		}
-		for(int i = from; i < nums.length; ++i) {
-			swap(nums, from, i);
-			cur.add(nums[from]);
-			helper(nums, from+1, cur, allList);
-			cur.remove(cur.size()-1);
-			swap(nums, from, i);
-		}
-	}
-	public void swap(int[] nums, int i, int j) {
-		int temp = nums[i];
-		nums[i] = nums[j];
-		nums[j] = temp;
-	}
-    
-    
-    
-    
+        List<List<Integer>> list = new LinkedList<>();
+        boolean[] vis = new boolean[nums.length];
+        perm(nums, 0, new LinkedList<>(), list,vis);
+        
+        return list;
+    }
 }
