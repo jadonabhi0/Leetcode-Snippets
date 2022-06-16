2ad2e98edac036d514f6e48ba51efd57f12e2541
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-06-16 18:01:53 +0530

    Time: 2 ms (72.52%), Space: 41.2 MB (60.13%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.java
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -1,22 +1,33 @@
 class Solution {
+//     public int divide(int dividend, int divisor) {
+//         if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+       
+
+//         int sign = dividend > 0 && divisor > 0 ? -1 : 1;
+//         long num = Math.abs(dividend);
+//         long deno = Math.abs(divisor);
+//         int ans = 0;
+
+//         while(deno <= num){
+
+//             long temp = deno ; int count = 1;
+//             while((temp << 1) <= num){
+//                 temp = temp << 1;
+//                 count = count << 1;
+//             }
+//             ans += count;
+//             num = num - temp;
+//         }
+//         return sign * ans;
+//     }
+    
+    
+    
+    
     public int divide(int dividend, int divisor) {
-        if(dividend == 1<< 31 && divisor == -1) return Integer.MAX_VALUE;
-        boolean sign = (dividend>=0) == (divisor>= 0) ? true : false;
-        dividend = Math.abs(dividend);
-        divisor = Math.abs(divisor);
-        int result=0;
-        while(dividend - divisor >= 0)
-        {
-            int count = 0;
-            while(dividend - (divisor <<1 << count)>= 0){
-                count++;
-            }
-            result += 1 << count;
-            dividend -= divisor << count;
-            
-        }
-        return sign?result: - result;
-        
-        
+    if(dividend == Integer.MIN_VALUE && divisor == -1){
+        return Integer.MAX_VALUE;
     }
+    return dividend / divisor;
+}
 }
