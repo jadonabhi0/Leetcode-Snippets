d42aa899a025e1ac13ca855945c330f033489ddc
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-06-17 10:53:50 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (53.41%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,23 +1,22 @@
 class Solution {
-    
-    
     public int numTrees(int n) {
+        int[] array = new int[n+1];
         
-       int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
+        array[0] = 1;
+        array[1] = 1;
         
-        for(int  i = 2 ; i <= n ; i++){
-            
+        for(int i = 2 ; i <= n ; i++){
             int l = 0;
             int r = i-1;
             
             while(l <= i-1){
-                dp[i] += dp[l]*dp[r];
+                
+                array[i] += array[l]*array[r];
                 l++;
                 r--;
+                
             }
         }
-        return dp[n];
+        return array[n];
     }
 }
