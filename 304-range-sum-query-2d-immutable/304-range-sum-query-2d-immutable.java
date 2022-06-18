9a2b3d06463635bd3cf48483bc0c9aa9c48ef208
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-06-18 21:33:32 +0530

    Time: 148 ms (90.29%), Space: 65.7 MB (93.48%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -1,33 +1,32 @@
 class NumMatrix {
 
-	int[][] dp;
-
-	public NumMatrix(int[][] matrix) {
-		dp = new int[matrix.length][matrix[0].length];
-		populateArray(matrix, dp);
-	}
-
-	private void populateArray(int[][] arr, int[][] dp) {
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[0].length; j++) {
-				if (j == 0) {
-					dp[i][j] = arr[i][j];
-				} else {
-					dp[i][j] = arr[i][j] + dp[i][j - 1];
-				}
-			}
-		}
-	}
-
-	public int sumRegion(int row1, int col1, int row2, int col2) {
-		if (row2 >= dp.length || col2 >= dp[0].length || row1 < 0 || col1 < 0)
+    int [][] dp ;
+    int sum = 0;
+    public NumMatrix(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        dp = new int[row][col];
+        
+        for(int i = 0 ; i < row ; i++){
+            for(int j = 0 ; j < col ; j++){
+                if(j == 0){
+                    dp[i][j] = matrix[i][j];
+                }else{
+                    dp[i][j] = matrix[i][j] + dp[i][j-1];
+                }
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        sum = 0;
+        if (row2 >= dp.length || col2 >= dp[0].length || row1 < 0 || col1 < 0)
 			return -1;
-		int sum = 0;
 		for (int i = row1; i <= row2; i++) {
 			sum = sum + (dp[i][col2] - (col1 > 0 ? dp[i][col1 - 1] : 0));
 		}
 		return sum;
-	}
+    }
 }
 
 /**
