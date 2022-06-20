b9348102d9ab5219f99e1159bf07283343a08d6c
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-06-20 22:35:32 +0530

    Time: 49 ms (37.42%), Space: 64.9 MB (27.61%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    
+   static class TrieNode {
+    HashMap<Character, TrieNode> next = new HashMap<>();
+    int depth;
+    }
+    
+    
+    public int minimumLengthEncoding(String[] words) {
+        TrieNode root = new TrieNode();
+        List<TrieNode> leaves = new  ArrayList<TrieNode>();
+        for (String w : new HashSet<>(Arrays.asList(words))) {
+            TrieNode cur = root;
+            for (int i = w.length() - 1; i >= 0; --i) {
+                char j = w.charAt(i);
+                if (!cur.next.containsKey(j)) cur.next.put(j, new TrieNode());
+                cur = cur.next.get(j);
+            }
+            cur.depth = w.length() + 1;
+            leaves.add(cur);
+        }
+        int res = 0;
+        for (TrieNode leaf : leaves) if (leaf.next.isEmpty()) res += leaf.depth;
+        return res;
+    }
+}
