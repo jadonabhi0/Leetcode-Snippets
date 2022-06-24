918db6fddab916df5b2a9ed995502d5550fd7b60
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-06-24 23:56:35 +0530

    Time: 3 ms (98.99%), Space: 57.5 MB (56.57%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -0,0 +1,23 @@
+class Solution {
+public boolean isPossible(int[] target) {
+
+    long sum = 0;
+    int max = 0;
+    
+    for(int i = 0; i<target.length ; i++)
+    {
+        sum += target[i];
+        
+        if(target[max] < target[i])max = i;
+    }
+    
+    long diff = sum - target[max];
+    
+    if(diff == 1 || target[max] == 1)return true;
+    
+    if(diff > target[max] || diff == 0 || target[max]%diff == 0)return false;
+    target[max] %=diff;
+    return isPossible(target);
+}
+}
+
