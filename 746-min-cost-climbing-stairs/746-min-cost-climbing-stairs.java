20d4a929e877a7c948f9dd1fb4f891aba5422ef7
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-07-10 19:05:53 +0530

    Time: 1 ms (89.45%), Space: 43.4 MB (64.01%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,18 +1,20 @@
 class Solution {
     
-    public int helper(int n, int[] cost, int[] dp){
-        
-        if(n <= 1) return dp[n] = cost[n];
-        
-        if(dp[n] != 0) return dp[n];
-        
-        return dp[n] = Math.min(helper(n - 1, cost, dp), helper(n - 2, cost, dp)) + cost[n];
+     public static int helper_tabu(int n, int[] cost, int[] dp){
+        if(cost.length == 2) return Math.min(cost[0],cost[1]);
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2 ; i < n ; i++){
+            dp[i] = Math.min(dp[i-1],dp[i-2]) + cost[i];
+        }
+        return Math.min(dp[n-1], dp[n-2]);
     }
     
     public int minCostClimbingStairs(int[] cost) {
-        if(cost.length == 2) return Math.min(cost[0],cost[1]);
         int[] dp = new int[cost.length];
-        helper(cost.length - 1, cost, dp);
-        return Math.min(dp[cost.length - 1] , dp[cost.length - 2]);
+        return helper_tabu(cost.length , cost, dp);
+        
     }
 }
