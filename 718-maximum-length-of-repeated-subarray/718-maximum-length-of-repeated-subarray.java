027f529ddf29bc5d07ec27e236e647bd6196fee6
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-08-06 01:19:43 +0530

    Time: 117 ms (18.89%), Space: 77.5 MB (67.37%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i = 1 ; i < dp.length; i++){
             for(int j = 1 ; j < dp[0].length ; j++){
                 boolean flag = nums1[i-1] == nums2[j-1];
-                if(flag) dp[i][j] = dp[i-1][j-1] + 1;
+                if(flag) dp[i][j] = ++dp[i-1][j-1];
                 ovmax = Math.max(ovmax, dp[i][j]);
             }
         }
