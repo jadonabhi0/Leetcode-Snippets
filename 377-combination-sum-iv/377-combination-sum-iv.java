354a110792c8870fbc75a30e0427972f60dc91ad
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-08-05 21:47:04 +0530

    Time: 3 ms (36.49%), Space: 42.4 MB (7.01%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,14 +1,15 @@
 class Solution {
+    HashMap<Integer, Integer> map = new HashMap<>();
     public int combinationSum4(int[] nums, int target) {
-        int[] dp = new int[target+1];
-        dp[0] = 1;
-        for(int i = 1 ; i < dp.length ; i++){
-            for(int j = 0 ; j < nums.length ; j++){
-                if(i >= nums[j]){
-                    dp[i] += dp[i-nums[j]];
-                }
+        if(target == 0) return 1;
+        if(map.containsKey(target)) return map.get(target);
+        int ans = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(target >= nums[i]){
+                ans += combinationSum4(nums, target - nums[i]);
+                map.put(target, ans);
             }
         }
-        return dp[dp.length-1];
+        return ans;
     }
 }
