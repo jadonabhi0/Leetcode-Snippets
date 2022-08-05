e2cb727c2fc82e27a577773eb3005f2788e88ba5
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-08-05 23:32:18 +0530

    Time: 1 ms (99.11%), Space: 40.8 MB (91.42%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,22 +1,24 @@
-public class Solution {
+class Solution {
+    public static boolean helper_rec(String s1, String s2, String s3, int i, int j, Boolean[][] dp){
+        if (i == s1.length() && j == s2.length()) return true;
+        if (dp[i][j] != null) return dp[i][j];
+        if(i < s1.length() && s1.charAt(i) == s3.charAt(i+j)){
+            boolean f1 = helper_rec(s1, s2, s3, i+1, j, dp);
+            dp[i][j] = f1;
+            if (f1) return true;
+
+        }
+
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(i+j)){
+            boolean f1 = helper_rec(s1, s2, s3, i, j+1, dp);
+            dp[i][j] = f1;
+            if (f1) return true;
+        }
+        dp[i][j] = false;
+        return false;
+    }
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s3.length() != s1.length() + s2.length()) {
-            return false;
-        }
-        boolean dp[][] = new boolean[s1.length() + 1][s2.length() + 1];
-        for (int i = 0; i <= s1.length(); i++) {
-            for (int j = 0; j <= s2.length(); j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = true;
-                } else if (i == 0) {
-                    dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
-                } else if (j == 0) {
-                    dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
-                } else {
-                    dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
-                }
-            }
-        }
-        return dp[s1.length()][s2.length()];
+        if(s1.length() + s2.length() != s3.length()) return false;
+        return helper_rec(s1, s2, s3, 0, 0, new Boolean[s1.length()+1][s2.length()+1]);
     }
 }
