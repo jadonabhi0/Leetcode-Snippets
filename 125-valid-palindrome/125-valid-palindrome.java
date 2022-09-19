a57209edea4f2d49c5780dc446ba4dda2b3df381
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-09-19 15:17:35 +0530

    Time: 992 ms (19.14%), Space: 564.2 MB (5.01%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,20 +1,23 @@
-class Solution {
-     public boolean isPalindrome(String s) {
-        int start = 0; 
-        int end = s.length() - 1;
-        while(start <= end) {
-            while(start <= end && !Character.isLetterOrDigit(s.charAt(start))) {
-                start++;
-            }
-            while(start <= end && !Character.isLetterOrDigit(s.charAt(end))) {
-                end--;
-            }
-            if(start <= end && Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
-                return false;
-            }
-            start++;
-            end--;
+ class Solution {
+     
+    public boolean helper(String s){
+        int l = 0;
+        int r = s.length() -1;
+        while(l < r){
+            if(s.charAt(l) != s.charAt(r)) return false;
+            l++;r--;
         }
         return true;
     }
+    public boolean isPalindrome(String s) {
+        String str = "";
+        for(int i = 0 ;i < s.length() ; i++){
+            int v = s.codePointAt(i);
+            if(v >= 97 && v <= 122 || v >= 65 && v <= 90 || v >= 48 && v <= 57){
+                str += s.charAt(i)+"";
+            }
+        }
+        str = str.toLowerCase();
+        return helper(str);
+    }  
 }
