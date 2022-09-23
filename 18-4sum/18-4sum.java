43cedc707bc62d1585c272ccda993851ef08df21
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-09-23 16:17:54 +0530

    Time: 28 ms (60.01%), Space: 44.5 MB (69.44%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,25 +1,24 @@
 class Solution {
-    List<List<Integer>> list = new LinkedList<>();
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        if(nums.length < 4) return list;
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i = 0 ; i < nums.length ; i++){ // finding first element.
+        for(int i = 0 ; i < nums.length ; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            for(int j = i+1 ; j < nums.length ; j++){ // finding second element.
+            for(int j = i+1; j < nums.length ; j++){
                 if(j > i+1 && nums[j] == nums[j-1]) continue;
-                long t = (long)target - nums[i] - nums[j];
                 int k = j+1;
                 int l = nums.length-1;
+                long t = (long)target - nums[i] - nums[j];
                 while(k < l){
-                    if(nums[k] + nums[l] == t){
+                    if(t == nums[k]+nums[l]){
                         list.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
                         while(k < l && nums[k] == nums[k+1]) k++;
                         while(k < l && nums[l] == nums[l-1]) l--;
                         k++;
                         l--;
                     }
-                    else if(nums[l] + nums[k] > t) l--;
-                    else k++;
+                    else if(t > nums[k]+nums[l]) k++;
+                    else l--;
                 }
             }
         }
