54dbf9f613cc8fae96e98c49ad4cf8cc8a975d4a
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-09-23 14:48:32 +0530

    Time: 31 ms (70.70%), Space: 58 MB (82.31%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,24 +1,26 @@
 class Solution {
-    List<List<Integer>> list = new LinkedList<>();
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new LinkedList<>();
         Arrays.sort(nums);
-        for(int i = 0 ; i < nums.length ; i++){
+        for(int i = 0 ; i < nums.length-1 ; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
             int a = nums[i];
-            int left = i+1;
-            int right = nums.length-1 ;
-            while(left < right){
-                if(nums[left] + nums[right] == -a){
-                    list.add(Arrays.asList(a, nums[left], nums[right]));
-                    while(left < right && nums[left] == nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right-1]) right--;
-                    left++;
-                    right--;
+            int l = i+1;
+            int r = nums.length -1;
+            
+            while(l < r){
+                if(nums[l] + nums[r] == -a){
+                    list.add(Arrays.asList(a, nums[l], nums[r]));
+                    while(l < r && nums[l] == nums[l+1]) l++;
+                    while(l < r && nums[r] == nums[r-1]) r--;
+                    l++;
+                    r--;
                 }
-                else if(nums[left] + nums[right] > -a) right--;
-                else if(nums[left] + nums[right] < -a) left++;
-            }    
+                else if(nums[l] + nums[r] > -a) r--;
+                else if(nums[l] + nums[r] < -a) l++;
+            }
         }
-        return list;
+          return list;  
+        }
+        
     }
-}
