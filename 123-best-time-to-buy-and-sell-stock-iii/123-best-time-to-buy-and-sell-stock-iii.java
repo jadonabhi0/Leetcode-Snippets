fbfecc232dc594dbc3c51d6b670f4020ff4da5ea
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-09-28 12:57:02 +0530

    Time: 6 ms (78.41%), Space: 81.7 MB (46.29%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,42 +1,38 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[] dp1 = new int[prices.length];
-        int[] dp2 = new int[prices.length];
-
-//      [3,3,5,0,0,3,1,4]
-
-        int pastMin = Integer.MAX_VALUE;
+        int n = prices.length;
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+        
+        int ovmax = 0;
         int profit = 0;
-        int maxProfit = 0;
-
-
-        for(int i = 0 ; i < prices.length ; i++){
-            pastMin = Math.min(pastMin, prices[i]);
-            profit = prices[i] - pastMin;
-            maxProfit = Math.max(profit, maxProfit);
-            dp1[i] = maxProfit;
+        int min = Integer.MAX_VALUE;
+        
+        
+        for(int i = 0 ; i < n ; i++){
+            min = Math.min(min, prices[i]);
+            profit = prices[i] - min;
+            ovmax = Math.max(ovmax, profit);
+            dp1[i] = ovmax;
         }
-
+        
+        int max = Integer.MIN_VALUE;
         profit = 0;
-        maxProfit = 0;
-//       [3,3,5,0,0,3,1,4]
-        int futureMax = Integer.MIN_VALUE;
-        for(int i = prices.length-1 ; i >= 0 ; i--){
-            futureMax = Math.max(futureMax, prices[i]);
-            profit = futureMax - prices[i];
-            maxProfit = Math.max(profit,maxProfit);
-            dp2[i] = maxProfit;
+        ovmax = 0;
+        
+        for(int i = n - 1 ; i >= 0 ; i--){
+            max = Math.max(max, prices[i]);
+            profit = max - prices[i];
+            ovmax = Math.max(profit, ovmax);
+            dp2[i] = ovmax;
         }
-
-        // System.out.println(Arrays.toString(dp1));
-        // System.out.println(Arrays.toString(dp2));
-
-        int overAllProfit = 0;
-
-        for(int i = 0 ; i < dp1.length ; i++){
-            overAllProfit = Math.max(overAllProfit, dp1[i]+dp2[i]);
+        
+        ovmax = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            ovmax = Math.max(ovmax, dp1[i]+dp2[i]);
         }
-
-        return overAllProfit;
+        
+        return ovmax;
     }
 }
