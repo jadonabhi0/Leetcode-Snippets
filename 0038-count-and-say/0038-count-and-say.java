38d2b4a03606b41b0943de2d66ea10227ade0bb2
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-10-18 23:34:18 +0530

    Time: 31 ms (31.83%), Space: 41.2 MB (90.21%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String countAndSay(int n) {
+        String res = "11";
+        if(n == 1) return "1";
+        // if(n == 2) return res;
+        for(int i = 3 ; i <= n ; i++){
+            StringBuffer sb = new StringBuffer();
+            int count = 1;
+            char ch = ' ';
+            for(int j = 0 ; j < res.length()-1 ; j++){
+                ch = res.charAt(j);
+                if(ch == res.charAt(j+1)){
+                    count++;
+                }else{
+                    sb.append(count);
+                    sb.append(ch);
+                    count = 1;
+                    
+                }
+            }
+            ch = res.charAt(res.length()-1);
+            sb.append(count);
+            sb.append(ch);
+            res = sb.toString();
+            
+        }
+        return res;
+    }
+}
