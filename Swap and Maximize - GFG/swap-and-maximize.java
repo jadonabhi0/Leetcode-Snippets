a38f294c8bd06b1511a2aa34fb0765118e523a59
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-11-17 21:44:44 +0530

    Added solution - LeetHub

---
+++ Swap and Maximize - GFG/swap-and-maximize.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+
+
+class GFG
+{
+    long maxSum(long arr[] ,int n)
+    {
+        Arrays.sort(arr);
+        long sum = 0;
+        
+        int i = 0;
+        int j = n-1;
+        
+        while(i < j){
+            sum += Math.abs(arr[i] - arr[j]);
+            sum += Math.abs(arr[i+1] - arr[j]);
+            i++;
+            j--;
+        }
+        
+        sum += Math.abs(arr[0] - arr[i]);
+        
+        return sum;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main (String[] args) throws IOException{
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while(testcases-- > 0){
+            int n=Integer.parseInt(br.readLine());
+//            String line = br.readLine();
+//            String[] a2 = line.trim().split("\\s+");
+//            int n =Integer.parseInt(a2[0]);
+//            int k =Integer.parseInt(a2[1]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long arr[]=new long[n];
+            for(int  i=0;i<n;i++)
+            {
+                arr[i]=Long.parseLong(a1[i]);
+            }
+            GFG ob=new GFG();
+            //ArrayList<Long> ans=ob.smallestDifferenceTriplet(a,b,c,n);
+            long ans=ob.maxSum(arr,n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
