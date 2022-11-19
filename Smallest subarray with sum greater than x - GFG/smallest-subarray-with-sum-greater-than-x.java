b4ef906ce15870747189355582845c0d886d7daf
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-11-19 13:19:39 +0530

    Added solution - LeetHub

---
+++ Smallest subarray with sum greater than x - GFG/smallest-subarray-with-sum-greater-than-x.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            // int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.smallestSubWithSum(a, n, m));
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+
+    public static int smallestSubWithSum(int a[], int n, int x) {
+        // Your code goes here 
+        int start = 0;
+        int sum = 0;
+        int ans = Integer.MAX_VALUE;
+        
+        for(int i = 0 ; i <= n ; i++){
+            
+            while(sum > x && start < i){
+                ans = Math.min(ans,i-start);
+                sum -= a[start++];
+            }
+            
+            if(i < n){
+                sum += a[i]; // adding
+            }
+            
+        }
+        
+        return ans;
+    }
+}
+
