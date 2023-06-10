fb9d3c10db4d7dd3b810f3627112c8118a6870f1
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2023-06-10 09:59:27 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.Rearrange(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void Rearrange(int a[], int n)
+    {
+       int lo = 0;
+       while(a[lo] < 0)lo++;
+       int hi = lo+1;
+       while(hi < a.length){
+           int temp = 0;
+           if(a[hi] < 0){
+               temp = a[hi];
+               for(int i = hi ; i > lo ; i--){
+                   a[i] = a[i-1];
+               }
+               a[lo] = temp;
+               lo++;
+           }
+           hi++;
+       }
+    }
+}
