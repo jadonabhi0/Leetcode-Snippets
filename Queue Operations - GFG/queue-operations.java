81b0c25b2a99b165e68c0fd332b4540fee939892
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2023-06-17 12:12:37 +0530

    Added solution - LeetHub

---
+++ Queue Operations - GFG/queue-operations.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+// Helper class Geeks to implement 
+// insert() and findFrequency()
+class Geeks{
+    
+    static HashMap<Integer, Integer> map ;
+    Geeks(){map = new HashMap<>();}
+    // Function to insert element into the queue
+    static void insert(Queue<Integer> q, int k){
+        q.offer(k);
+        map.put(k, map.getOrDefault(k, 0)+1);
+        
+    }
+    
+    // Function to find frequency of an element
+    // rteturn the frequency of k
+    static int findFrequency(Queue<Integer> q, int k){
+        return map.getOrDefault(k, -1);
+        
+    }
+    
+}
+
+//{ Driver Code Starts.
+
+// Driver class with driver code
+class GFG {
+    
+    // Driver code
+	public static void main (String[] args) {
+		
+		// Taking input using scanner class
+		Scanner sc = new Scanner(System.in);
+		int testcase = sc.nextInt();
+		
+		while(testcase-- > 0){
+		    // Declaring Queue
+		    Queue<Integer> q = new LinkedList<>();
+		    int n = sc.nextInt();
+		    
+		    // Invoking object of Geeks class
+		    Geeks obj = new Geeks();
+		    
+		    for(int i = 0;i<n;i++){
+		        int k = sc.nextInt();
+		        obj.insert(q, k);
+		    }
+		   
+		    int x = sc.nextInt();
+		    for(int i = 0;i<x;i++){
+		        int k = sc.nextInt();
+		        int f = obj.findFrequency(q, k);
+		        if(f != 0){
+		            System.out.println(f);
+		        }
+		        else{
+		            System.out.println("-1");
+		        }
+		    }
+		}
+	}
+}
+// } Driver Code Ends
