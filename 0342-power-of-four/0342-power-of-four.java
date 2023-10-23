d6ae5beb5f63151ca5c7d215c132b90347450096
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2023-10-23 21:49:28 +0530

    Time: 1 ms (76.31%), Space: 39.5 MB (58.82%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,7 @@
+class Solution {
+    
+    public boolean isPowerOfFour(int n) {
+        int mask = 0b01010101010101010101010101010101;
+        return n > 0 && (n & (n-1)) == 0 && (mask & n) > 0;   
+    }
+}
