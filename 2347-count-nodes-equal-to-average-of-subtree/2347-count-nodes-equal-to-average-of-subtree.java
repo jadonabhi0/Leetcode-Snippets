c23d5e637a2944af2e8488084c46a61c8cba9cfe
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2023-11-02 21:18:56 +0530

    Time: 0 ms (100%), Space: 42.8 MB (80.5%) - LeetHub

---
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    int result = 0;
+    
+    public int averageOfSubtree(TreeNode root) {
+        dfs(root);
+        return result;
+    }
+
+    private int[] dfs(TreeNode node) {
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.right);
+
+        int sum = left[0] + right[0] + node.val;
+        int count = left[1] + right[1] + 1;
+
+
+        if (sum / count == node.val) {
+            result++;
+        }
+
+        return new int[]{sum, count};
+    }
+}
