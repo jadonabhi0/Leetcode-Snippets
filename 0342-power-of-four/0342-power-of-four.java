45244494c3c1dc6c0fa62639d4b39411c12900ef
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2024-03-28 17:22:18 +0530

    Update 0342-power-of-four.java

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,7 +1,9 @@
 class Solution {
     
     public boolean isPowerOfFour(int n) {
+        
         int mask = 0b01010101010101010101010101010101;
         return n > 0 && (n & (n-1)) == 0 && (mask & n) > 0;   
+        
     }
 }
