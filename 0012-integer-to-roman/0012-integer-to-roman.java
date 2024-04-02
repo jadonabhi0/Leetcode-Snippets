ae6ff970b18a91e162862b5a17c68f62cb7a88a4
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2024-04-02 21:07:26 +0530

    Update 0012-integer-to-roman.java

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -5,6 +5,7 @@ class Solution {
         String[] tens ={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
         String[] hundreds = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
         String[] thousands = {"","M","MM","MMM"};
+        
         return thousands[num/1000]+hundreds[(num%1000)/100]+tens[(num%100)/10]+units[num%10];
     }
 }
