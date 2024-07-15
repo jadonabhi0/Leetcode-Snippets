8bb6f44a0c6577695bc5c5533f789ea41ba05627
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2024-07-15 22:33:54 +0530

    Time: 71 ms (41.72%), Space: 55.1 MB (86.75%) - LeetHub

---
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer, TreeNode> mp = new HashMap<>();
+        Set<Integer> hasParent = new HashSet<>();
+        
+        for (int i = 0; i < descriptions.length; i++) {
+            if (!mp.containsKey(descriptions[i][0]))
+                mp.put(descriptions[i][0], new TreeNode(descriptions[i][0]));
+            if (!mp.containsKey(descriptions[i][1]))
+                mp.put(descriptions[i][1], new TreeNode(descriptions[i][1]));
+            hasParent.add(descriptions[i][1]);
+        }
+        
+        TreeNode root = null;
+        for (int i = 0; i < descriptions.length; i++) {
+            if (descriptions[i][2] == 1) { // left
+                mp.get(descriptions[i][0]).left = mp.get(descriptions[i][1]);
+            } else { // right
+                mp.get(descriptions[i][0]).right = mp.get(descriptions[i][1]);
+            }
+            if (!hasParent.contains(descriptions[i][0])) {
+                root = mp.get(descriptions[i][0]);
+            }
+        }
+        return root;
+    }
+}
