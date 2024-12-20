e9a6907036d3a856d11b54850da14a5fe955f5af
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2024-12-20 22:09:47 +0530

    Time: 0 ms (100%), Space: 45 MB (22.14%) - LeetHub

---
+++ 3632-button-with-longest-push-time/3632-button-with-longest-push-time.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int buttonWithLongestTime(int[][] events) {
+        if(events.length == 1) return events[0][0];
+        
+        int idx = events[0][0];
+        int maxt = events[0][1];
+        
+        for(int i = 1 ; i < events.length ; i++){
+            int time = events[i][1] - events[i-1][1];
+
+            if(time > maxt){
+                maxt = time;
+                idx = events[i][0];
+            }
+            else if(maxt == time && idx > events[i][0]){
+                idx = events[i][0];
+            }
+                    
+        }
+
+        return idx;
+        
+    }
+}
