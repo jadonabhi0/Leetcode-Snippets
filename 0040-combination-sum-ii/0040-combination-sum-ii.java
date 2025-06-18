75a0b91dfa5d3ca247bf757a72cfc59d07504295
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2025-06-19 00:21:17 +0530

    Time: 2 ms (99.66%), Space: 43.4 MB (53.48%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,29 +1,41 @@
 class Solution {
-    
-    public void helper(int[] array, List<List<Integer>> ans, List<Integer> list, int tar, int idx){
-        if (tar == 0){
-            ans.add(new LinkedList<Integer>(list));
-            return;
-        }
-        if (tar < 0) return;
-
-        for (int i = idx ; i < array.length ; i++){
-            if (i > idx && array[i] == array[i-1]){
-                continue;
-            }
-            
-                list.add(array[i]);
-                helper(array, ans, list, tar-array[i],i+1);
-                list.remove(list.size()-1);
-            
-        }
-
-    }
-    
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> ans = new ArrayList<>();
-        helper(candidates, ans, new LinkedList<>(), target,0);
+        helper(candidates, target, 0, candidates.length, ans, new ArrayList<>());
         return ans;
+    }
+
+
+    private void helper(int[] candidates, int target, int idx, int n, List<List<Integer>> ans, List<Integer> lst){
+
+
+        if(target == 0){
+            ans.add(new ArrayList(lst));
+            return;
+        }
+
+        if(idx == n){
+            return;
+        }
+
+        for(int i = idx ; i < n ; i++){
+
+            if(i > idx && candidates[i] == candidates[i-1]){
+                continue;
+            }
+
+            if(candidates[i] <= target) {
+                lst.add(candidates[i]);
+                helper(candidates, target-candidates[i], i+1, n, ans, lst);
+                lst.remove(lst.size()-1);
+            }else{
+                break;
+            }
+
+           
+        }
+    
+        return;
     }
 }
