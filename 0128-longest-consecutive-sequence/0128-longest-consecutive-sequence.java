0989309e2a96fdb3b9771c67ace9f8c3c3881425
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2025-07-27 23:30:32 +0530

    Time: 31 ms (57.62%), Space: 67.3 MB (54.26%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -2,27 +2,22 @@ class Solution {
     public int longestConsecutive(int[] nums) {
 
         Set<Integer> set = new HashSet<>();
-        for (int i : nums)
-            set.add(i);
 
-        int ans = 0;
-        int cnt = 0;
+        for(int i : nums) set.add(i);
+        int ans = 0, cnt = 0;
 
-        for (int i : set) {
+        for(int i : set){
+            cnt = 0;
             int val = i;
-
-            // is x is the part of strek then x-1 should not be in set.
-            if (!set.contains(val - 1)) {
-                while (set.contains(val++)) {
-                    cnt++;
-                }
+            if(!set.contains(val-1)){
+                while(set.contains(val++)) cnt++;
             }
 
             ans = Math.max(ans, cnt);
-            cnt = 0;
 
         }
-        return ans;
 
+        return ans;
+    
     }
 }
