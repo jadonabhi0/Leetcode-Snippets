73c177cfc1ba74c7325c0f45b03f448a7478a481
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2025-08-18 23:36:22 +0530

    Time: 2 ms (83.17%), Space: 44.7 MB (64.19%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,32 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        helper(candidates, target, 0, candidates.length, ans, new ArrayList<>());
+        return generate(candidates, target, 0, new ArrayList<>(), new ArrayList<>());
+    }
+
+
+    private List<List<Integer>> generate(int[] cand, int target, int idx, List<Integer> lst,  List<List<Integer>> ans) {
+
+     if(target == 0){
+        ans.add(new ArrayList<>(lst));
         return ans;
+     }   
+
+     if(idx == cand.length || target < 0) return ans;
+
+     lst.add(cand[idx]);
+     generate(cand, target - cand[idx], idx, lst, ans);
+     lst.remove(lst.size() - 1);
+     generate(cand, target, idx + 1, lst, ans);
+     
+     return ans;   
+
     }
-
-    private void helper(int[] candidates, int target, int idx, int n, List<List<Integer>> ans, List<Integer> lst){
-
-        if(target == 0){
-            ans.add(new ArrayList(lst));
-            return;
-        }
-
-        if(idx == n){
-            return;
-        }
-        
-
-        if(target >= candidates[idx]){
-            lst.add(candidates[idx]);
-            helper(candidates, target-candidates[idx], idx, n, ans, lst);
-            lst.remove(lst.size()-1);
-        }
-        
-        helper(candidates, target, idx+1, n, ans, lst);
-
-        return;
-    }
-
-
 }
