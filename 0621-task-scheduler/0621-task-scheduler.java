ee11cafadfae2a4748c3c67071afd610fa4b75f0
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2026-05-01 13:53:07 +0530

    Time: 30 ms (30.8%), Space: 48.6 MB (11.62%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,54 +1,46 @@
 class Solution {
-
     class Task{
         char task;
         int fr;
         int sTime;
-
-        public Task(char task, int fr, int sTime){
+        Task(char task, int fr, int sTime){
             this.task = task;
             this.fr = fr;
             this.sTime = sTime;
         }
     }
 
-
     public int leastInterval(char[] tasks, int n) {
-
-        Map<Character, Integer> frMap = new HashMap<>();
-        PriorityQueue<Task> pq = new PriorityQueue<>((t1, t2) -> Integer.compare(t2.fr , t1.fr));
+        int interval = 0;
+        HashMap<Character, Integer> frMap = new HashMap<>();
+        PriorityQueue<Task> pq = new PriorityQueue<>((a, b) -> b.fr - a.fr);
         Queue<Task> q = new LinkedList<>();
 
-        // frq cnt
-        for(char ch : tasks) frMap.put(ch, frMap.getOrDefault(ch, 0) + 1);
+        for(char ch : tasks) frMap.put(ch, frMap.getOrDefault(ch, 0)+1);
+        for(char ch : frMap.keySet()) pq.add(new Task(ch, frMap.get(ch), 0));
 
-        // adding to pq 
-        for(char ch : frMap.keySet()) pq.offer(new Task(ch, frMap.get(ch), 0));
-        
-        int time = 0;
         while(!q.isEmpty() || !pq.isEmpty()){
-            time++;
-
-            Task sTask = null;  
+            interval++;
+            Task poll;
             if(!pq.isEmpty()){
-                sTask = pq.poll();
-                sTask.fr--;
-                if(sTask.fr > 0){
-                    sTask.sTime = time+n;
-                    q.offer(sTask);
+                poll = pq.poll();
+                poll.fr--;
+                if(poll.fr > 0){
+                    poll.sTime = interval + n;
+                    q.add(poll);
                 }
             }
 
             if(!q.isEmpty()){
-                if(q.peek().sTime == time){
+                if(q.peek().sTime == interval){
                     Task rm = q.poll();
-
                     pq.offer(rm);
                 }
             }
 
         }
 
-        return time;
+        return interval;
     }
+
 }
