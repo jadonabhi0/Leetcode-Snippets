51c5a68500f9fecdc5d48b3f2014fde20a7a7c31
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2026-05-01 18:37:54 +0530

    Time: 4 ms (42.79%), Space: 52.5 MB (22.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,28 @@
 class Solution {
+    int count = 0;
+
     public int numIslands(char[][] grid) {
-        int count=0, n = grid.length, m = grid[0].length;
-        int[][] vis = new int[n][m];
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] vis = new boolean[n][m];
+
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] == '1'){
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    count(grid, vis, i, j, n, m);
                     count++;
-                    dfs(grid, vis, i, j, n, m);
                 }
             }
         }
         return count;
     }
 
-
-    private void dfs(char[][] grid, int[][] vis, int row, int col, int n, int m){
-
-        if(row < 0 || row >= n || col < 0 || col >= m || vis[row][col] == 1 || grid[row][col] == '0') return ;
-
-        vis[row][col] = 1;
-        grid[row][col] = '0';
-        dfs(grid, vis, row-1, col, n, m);
-        dfs(grid, vis, row, col-1, n, m);
-        dfs(grid, vis, row+1, col, n, m);
-        dfs(grid, vis, row, col+1, n, m);
-
+    private void count(char[][] grid, boolean[][] vis, int i, int j, int n, int m){
+        if(i < 0 || j < 0 || i >= n || j >= m || vis[i][j] || grid[i][j] == '0')return;
+        vis[i][j] = true;
+        count(grid, vis, i+1, j, n, m);
+        count(grid, vis, i-1, j, n, m);
+        count(grid, vis, i, j+1, n, m);
+        count(grid, vis, i, j-1, n, m);
     }
 }
