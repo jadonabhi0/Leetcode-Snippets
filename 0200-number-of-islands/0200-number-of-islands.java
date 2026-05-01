437f4e947b281b7bd991200baf99288e47ac2bb6
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2026-05-01 18:40:34 +0530

    Time: 3 ms (86.89%), Space: 52.2 MB (69.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -4,12 +4,11 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        boolean[][] vis = new boolean[n][m];
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] == '1' && !vis[i][j]){
-                    count(grid, vis, i, j, n, m);
+                if(grid[i][j] == '1'){
+                    count(grid, i, j, n, m);
                     count++;
                 }
             }
@@ -17,12 +16,12 @@ class Solution {
         return count;
     }
 
-    private void count(char[][] grid, boolean[][] vis, int i, int j, int n, int m){
-        if(i < 0 || j < 0 || i >= n || j >= m || vis[i][j] || grid[i][j] == '0')return;
-        vis[i][j] = true;
-        count(grid, vis, i+1, j, n, m);
-        count(grid, vis, i-1, j, n, m);
-        count(grid, vis, i, j+1, n, m);
-        count(grid, vis, i, j-1, n, m);
+    private void count(char[][] grid, int i, int j, int n, int m){
+        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0' || grid[i][j] == '#')return;
+        grid[i][j] = '#';
+        count(grid, i+1, j, n, m);
+        count(grid, i-1, j, n, m);
+        count(grid, i, j+1, n, m);
+        count(grid, i, j-1, n, m);
     }
 }
