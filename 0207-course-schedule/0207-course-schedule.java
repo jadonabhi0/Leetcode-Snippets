bf937dd22370f533fecb181fb01d8f907cf826e2
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2026-05-02 23:16:10 +0530

    Time: 6 ms (73.38%), Space: 47.3 MB (31.86%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,71 +1,29 @@
 class Solution {
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-
-        //============ using DFS ================
-        // ArrayList<Integer>[] map = new ArrayList[numCourses];
-        // for(int i = 0; i < numCourses ; i++) map[i] = new ArrayList<>();
-
-        // for(int[] c : prerequisites){
-        //     map[c[0]].add(c[1]);
-        // }
-        // boolean [] vis = new boolean[numCourses];
-        // boolean [] pathVis = new boolean[numCourses];
-
-        // for(int i = 0 ; i < numCourses ; i++){
-        //     if(!vis[i]){
-        //         if(isGraphCyclic(map, vis, pathVis, i)){
-        //             return false;
-        //         }
-        //     }
-        // }
-        // return true;
-
-        // ===================== using BFS (Khans algo)======================
-
-
         ArrayList<Integer>[] map = new ArrayList[numCourses];
         for (int i = 0; i < numCourses; i++) map[i] = new ArrayList<>();
 
         int[] indeg = new int[numCourses];
-        for (int[] c : prerequisites) {
-            map[c[1]].add(c[0]);   // edge: b -> a
-            indeg[c[0]]++;         // a has an incoming edge
+        for(int[] it: prerequisites){
+            indeg[it[1]]++;
+            map[it[0]].add(it[1]);
         }
 
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (indeg[i] == 0) q.offer(i);
+        for(int i = 0 ; i < numCourses ; i++){
+            if(indeg[i] == 0) q.add(i);
         }
 
         int cnt = 0;
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int rm = q.poll();
             cnt++;
-            for (int nbr : map[rm]) {
-                indeg[nbr]--;
-                if (indeg[nbr] == 0) q.offer(nbr);
+            for(int i : map[rm]){
+                indeg[i]--;
+                if(indeg[i] == 0) q.offer(i);
             }
         }
 
-        return cnt == numCourses;
-    }
-        
-
-
-    private boolean isGraphCyclic(ArrayList<Integer>[] graph, boolean[] vis, boolean[] pathVis, int node) {
-        vis[node] = true;
-        pathVis[node] = true;
-
-        for (int nbr : graph[node]) {
-            if (!vis[nbr]) {
-                if (isGraphCyclic(graph, vis, pathVis, nbr)) return true;
-            } else if (pathVis[nbr]) {
-                return true; // cycle detected
-            }
-        }
-
-        pathVis[node] = false; // backtrack
-        return false;
+        return numCourses == cnt;
     }
 }
