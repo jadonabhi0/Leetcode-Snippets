f42f045734d247de15dc74d66502ad34aba8c298
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2026-07-25 19:11:47 +0530

    Time: 172 ms (6.02%), Space: 91.6 MB (5.29%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,44 +1,53 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-
-        Stack<Integer> st = new Stack<>();
-        int n = heights.length;
-        int[] ls = new int[n];
-        int[] rs = new int[n];
-
-        // find the right smaller
-        rs[n-1] = n;
-        st.push(n-1);
-
-        for(int i = n-2 ; i >= 0 ; i--){
-            while(!st.isEmpty() && heights[i] <= heights[st.peek()]) st.pop();
-            rs[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
-        } 
-
-        st.clear();
-
-        // find left smaller
-
-        ls[0] = -1;
-        st.push(0);
-
-        for(int i = 1 ; i < n ; i++){
-            while(!st.isEmpty() && heights[i] <= heights[st.peek()]) st.pop();
-            ls[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
-        } 
-        // cal area
-
-        int maxA = 0;
+    public int largestRectangleArea(int[] ht) {
+        int n = ht.length;
+        int[] ls = getNextSmaller(ht, 'l');
+        int[] rs = getNextSmaller(ht, 'r');
+        System.out.println(Arrays.toString(ls));
+        System.out.println(Arrays.toString(rs));
+        int maxArea = 0;
 
         for(int i = 0 ; i < n ; i++){
-            int wid = rs[i] - ls[i] - 1;
-            int area = wid * heights[i];
-            maxA = Math.max(maxA, area);
+            int width = rs[i] - ls[i]-1;
+            int area = width*ht[i];
+
+            if(area > maxArea){
+                maxArea = area;
+            }
+        }
+
+        return maxArea;
+        
+    }
+
+    private int[] getNextSmaller(int[] arr, char dir){
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[arr.length];
+
+        if(dir == 'l'){
+            st.push(0);
+            ans[0] = -1;
+            for(int i = 1 ; i < arr.length ; i++){
+                while(!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
+                ans[i] = st.isEmpty() ? -1 : st.peek();
+                st.push(i);
+            }
+        }
+        
+        if(dir == 'r'){
+            st.push(arr.length-1);
+            ans[arr.length-1] = arr.length;
+
+
+            for(int i = arr.length-2 ; i >= 0 ; i--){
+                while(!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
+                ans[i] = st.isEmpty() ? arr.length : st.peek();
+                st.push(i);
+            }
 
         }
 
-        return maxA;
+        return ans;
     }
+
 }
