bf20f19dd0bbd11a4829f0b5c1ace065b1a3ae05
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2026-07-27 00:25:32 +0530

    Time: 2 ms (90.46%), Space: 44.2 MB (56.79%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,43 +1,45 @@
 class Solution {
-    int[][] dir = new int[][]{{1,0}, {-1,0}, {0, 1}, {0,-1}};
+    int[][] dir = new int[][] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         Queue<int[]> q = new LinkedList<>();
         int count = 0;
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] == 2) q.offer(new int[]{i,j});
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) q.offer(new int[] { i, j });
             }
         }
+
 
         while(!q.isEmpty()){
             int size = q.size();
-            boolean hasRottenInThisMin = false;
+            boolean hasRottenOrange = false;
             while(size-->0){
                 int[] rm = q.poll();
                 for(int[] d : dir){
-                    int ni = rm[0] + d[0];
-                    int nj = rm[1] + d[1];
-                    if(ni < 0 || ni >= n || nj < 0 || nj >= m || grid[ni][nj] != 1) continue;
+                    int ni = d[0] + rm[0];
+                    int nj = d[1] + rm[1];
+                    if(ni < 0 || ni == n || nj < 0 || nj== m || grid[ni][nj] != 1) continue;
                     grid[ni][nj] = 2;
+                    hasRottenOrange = true;
                     q.offer(new int[]{ni, nj});
-                    hasRottenInThisMin = true;
                 }
             }
-            if(hasRottenInThisMin) count++;
+            if(hasRottenOrange) count++;
         }
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] == 1) return -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1){
+                    return -1;
+                }
             }
         }
 
-        return count;
+
+        return count++;
     }
-
-    
-
 }
