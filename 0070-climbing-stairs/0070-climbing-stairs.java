1bef61b7b3c5942c10cc6cd9e169fc8c0aa6fe2e
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2026-08-04 18:03:01 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,13 @@
 class Solution {
+
     Map<Integer, Integer> dp = new HashMap<>();
 
     public int climbStairs(int n) {
-        if(n < 0) return 0;
-        if(n == 0 || n == 1) return 1;
 
         if(dp.containsKey(n)) return dp.get(n);
-
+        if(n <= 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
         int a = climbStairs(n-1);
         int b = climbStairs(n-2);
         dp.put(n, a+b);
